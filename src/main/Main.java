0a2854346e9a7e932794eaf60917e45c2833b752
Dennis Silva <72114542+dennisfsilva@users.noreply.github.com>
2023-01-20 11:14:31 +0000

    updated

--- src/main/Main.java
+++ src/main/Main.java
@@ -48,14 +48,14 @@ public class Main {
 
     static void test() {
         System.out.println("Let's test your programming knowledge.");
-        while(true) {
+        while(scanner.hasNextLine()) {
             System.out.println("Why do we use methods?");
             System.out.println("1. To repeat a statement multiple times.");
             System.out.println("2. To decompose a program into several small subroutines.");
             System.out.println("3. To determine the execution time of a program.");
             System.out.println("4. To interrupt the execution of a program.");
 
-            String answer = scanner.nextLine();
+            String answer = scanner.next();
             if (answer.equals("2")) {
                 System.out.println("Congratulations, that's correct!");
                 break;
